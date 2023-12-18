f8b27f28c14599003c9723a6cf54c68c03eb50c7
rabopuk <rabopuk@gmail.com>
2023-12-18 18:21:47 +0400

    feat: api functionality

--- index.js
+++ index.js
@@ -2,30 +2,98 @@ import http from "node:http";
 import fs from "node:fs/promises";
 
 const PORT = 8080;
+const COMEDIANS = './comedians.json';
+const CLIENTS = './clients.json';
 
-http
-  .createServer(async (req, res) => {
-    if (req.method === "GET" && req.url === "/comedians") {
+const checkFiles = async () => {
+  try {
+    await fs.access(COMEDIANS);
+  } catch (error) {
+    console.error(`Файл ${COMEDIANS} не найден`);
+    return false;
+  }
+
+  try {
+    await fs.access(CLIENTS);
+  } catch (error) {
+    await fs.writeFile(CLIENTS, JSON.stringify([]));
+    console.log(`Файл ${CLIENTS} был создан`);
+    return false;
+  }
+
+  return true;
+};
+
+const sendData = (res, data) => {
+  res.writeHead(200, {
+    "Content-Type": "text/json; charset=utf-8",
+    "Access-Control-Allow-Origin": "*",
+  });
+
+  res.end(data);
+};
+
+const sendError = (res, statusCode, errMessage) => {
+  res.writeHead(statusCode, {
+    "Content-Type": "text/plain; charset=utf-8",
+  });
+
+  res.end(errMessage);
+};
+
+const startServer = async () => {
+  if (!(await checkFiles())) {
+    return;
+  }
+
+  http
+    .createServer(async (req, res) => {
       try {
-        const data = await fs.readFile('comedians.json', 'utf-8');
-        res.writeHead(200, {
-          "Content-Type": "text/json; charset=utf-8",
-          "Access-Control-Allow-Origin": "*",
-        });
-        res.end(data);
-      } catch (error) {
-        res.writeHead(500, {
-          "Content-Type": "text/plain; charset=utf-8",
-        });
-        res.end(`Ошибка сервера: ${error}`);
-      }
-    } else {
-      res.writeHead(404, {
-        "Content-Type": "text/plain; charset=utf-8",
-      });
-      res.end('Not found');
-    }
-  })
-  .listen(PORT);
+        const segments = req.url.split('/').filter(Boolean);
+        // console.log('segments: ', segments);
 
-console.log(`Сервер запущен на http://localhost:${PORT}`);
+        if (req.method === "GET" && segments[0] === "comedians") {
+          const data = await fs.readFile(COMEDIANS, 'utf-8');
+
+          if (segments.length === 2) {
+            const comedian = JSON.parse(data).find(c => c.id === segments[1]);
+
+            if (!comedian) {
+              sendError(res, 404, 'Комик не найден');
+              return;
+            }
+
+            sendData(res, JSON.stringify(comedian));
+            // return;
+          }
+
+          sendData(res, data);
+          return;
+        }
+
+        if (req.method === "POST" && segments[0] === 'clients') {
+          // POST /clients
+          // Добавление клиента
+        }
+
+        if (req.method === "GET" && segments[0] === 'clients' && segments.length === 2) {
+          // GET /clients/:ticket
+          // Получение клиента по номеру билета
+        }
+
+        if (req.method === "PATCH" && segments[0] === 'clients' && segments.length === 2) {
+          // PATCH /clients/:ticket
+          // Редактирование/обновление клиента по номеру билета
+        }
+
+        sendError(res, 404, 'Not found');
+      } catch (error) {
+        sendError(res, 500, `Ошибка сервера: ${error}`);
+      }
+    })
+    .listen(PORT);
+
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+};
+
+startServer();
